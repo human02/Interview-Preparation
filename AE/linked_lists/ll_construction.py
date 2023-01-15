f70caa054ce4a1c33c4c5b4eec331e1c85161af5
Surya-Dev <aaravchandra02@gmail.com>
2023-01-14 16:51:15 -0800

    add init code

--- AE/linked_lists/ll_construction.py
+++ AE/linked_lists/ll_construction.py
@@ -21,3 +21,50 @@ you may want to look at the various function signatures in a typed language
 like Java or TypeScript to get a better idea of what each input parameter is.
   
 """
+# This is an input class. Do not edit.
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
+# Feel free to add new properties and methods to the class.
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def setHead(self, node):
+        # Write your code here.
+        pass
+
+    def setTail(self, node):
+        # Write your code here.
+        pass
+
+    def insertBefore(self, node, nodeToInsert):
+        # Write your code here.
+        pass
+
+    def insertAfter(self, node, nodeToInsert):
+        # Write your code here.
+        pass
+
+    def insertAtPosition(self, position, nodeToInsert):
+        # Write your code here.
+        pass
+
+    def removeNodesWithValue(self, value):
+        # Write your code here.
+        pass
+
+    def remove(self, node):
+        # Write your code here.
+        pass
+
+    def containsNodeWithValue(self, value):
+        # Write your code here.
+        pass
