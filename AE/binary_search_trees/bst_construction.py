7c9e58cabca705a60c7f10d86871f37faec609aa
Surya-Dev <aaravchandra02@gmail.com>
2023-01-22 22:56:55 -0800

    add comments

--- AE/binary_search_trees/bst_construction.py
+++ AE/binary_search_trees/bst_construction.py
@@ -16,12 +16,14 @@ class BST:
         currNode = self
         while True:
             if value < currNode.value:
+                # reached leaf node on left tree
                 if currNode.left is None:
                     currNode.left = BST(value)
                     break
                 else:
                     currNode = currNode.left
             else:
+                # reached leaf node on right tree
                 if currNode.right is None:
                     currNode.right = BST(value)
                     break
@@ -32,7 +34,6 @@ class BST:
 
 # avg case -> time O(log(n)), space - O(1)
 # worst case -> time O(n), space - O(1)
-
 
     def contains(self, value):
         currNode = self
@@ -49,6 +50,7 @@ class BST:
 # avg case -> time O(log(n)), space - O(1)
 # worst case -> time O(n), space - O(1)
 
+
     def remove(self, value, parentNode=None):
         currentNode = self
         while currentNode is not None:
@@ -59,9 +61,11 @@ class BST:
                 parentNode = currentNode
                 currentNode = currentNode.right
             else:
-                # case where the node is not a leaf node (2 children node case)
+                # case where the node is not a leaf node (2 children node are there case)
                 if currentNode.left is not None and currentNode.right is not None:
+                    # we need smallest value of the right subtree
                     currentNode.value = currentNode.right.getMinValue()
+                    # now remove that node
                     currentNode.right.remove(currentNode.value, currentNode)
                 # root node case (no Parent node)
                 elif (parentNode is None):
@@ -75,6 +79,7 @@ class BST:
                         currentNode.right = currentNode.right.right  # the statement order matters
                     else:
                         pass  # case with only one node tree
+                #
                 elif parentNode.left == currentNode:
                     parentNode.left = currentNode.left if currentNode.left is not None else currentNode.right
                 elif parentNode.right == currentNode:
