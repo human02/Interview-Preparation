35fc88c1600647b66dda565cc3d1cac94b9cd40f
Surya-Dev <aaravchandra02@gmail.com>
2023-02-02 12:56:47 -0800

    soln (strings) - minimum characters of words

--- AE/strings/min_char_of_words.py
+++ AE/strings/min_char_of_words.py
@@ -1,2 +1,35 @@
 """
 """
+
+
+def minimumCharactersForWords(words):
+    resultDict = {}
+    result = []
+
+    for i in words:
+        helperCount(i, resultDict)
+
+    # create list using freq dict - resultDict
+    for key, val in resultDict.items():
+        for idx in range(val):
+            result.append(key)
+    # print(result)
+    return result
+
+
+def helperCount(str, res):
+    currReqDict = {}
+    # get current word's char requirement
+    for val in str:
+        if val in currReqDict.keys():
+            currReqDict[val] += 1
+        else:
+            currReqDict[val] = 1
+
+    # Compare current word reqs and add necessary in result dict
+    for key, value in currReqDict.items():
+        if key in res:
+            if currReqDict[key] > res[key]:
+                res[key] = currReqDict[key]
+        else:
+            res[key] = currReqDict[key]
