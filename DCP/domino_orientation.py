bcfd583c10f99568e9084b8cb13925d86ab3fcf8
Surya-Dev <aaravchandra02@gmail.com>
2023-02-08 22:32:32 -0800

    soln - push dominoes (dominoes orientation)

--- DCP/domino_orientation.py
+++ DCP/domino_orientation.py
@@ -12,3 +12,53 @@ For example, given the string .L.R....L, you should return LL.RRRLLL.
 Given the string ..R...L.L, you should return ..RR.LLLL. 
 
 """
+
+# O(n) time | O(n) space
+def push_dominoes(dominoes):
+    n = len(dominoes)
+
+    # It stores the forces experienced at each domino
+    forces = [0] * n
+
+    # finding force for R right-side
+    f = 0
+    for i in range(n):
+        if dominoes[i] == "R":
+            f = n
+        elif dominoes[i] == "L":
+            f = 0
+        else:
+            f = max(
+                f - 1, 0
+            )  # (f-1) as each . will reduce the force by 1 unit, max to prevent -1 case
+        forces[i] += f
+
+    # finding force for L left-side
+    f = 0
+    for i in range(n - 1, -1, -1):
+        if dominoes[i] == "L":
+            f = n
+        elif dominoes[i] == "R":
+            f = 0
+        else:
+            f = max(
+                f - 1, 0
+            )  # (f-1) as each . will reduce the force by 1 unit, max to prevent -1 case
+        forces[i] -= f
+
+    # list to store the final output
+    result = ""
+
+    # last iteration to check aggregate force to tell the fall side or standing.
+    for i in range(n):
+        if forces[i] == 0:
+            result += "."
+        elif forces[i] > 0:
+            result += "R"
+        else:
+            result += "L"
+    return result
+
+
+print(push_dominoes(".L.R....L"))  # LL.RRRLLL.
+print(push_dominoes("..R...L.L"))  # ..RR.LLLL.
