631722723b8b0b4ddcf94ea3c6c4db5d0c8090e5
Surya-Dev <aaravchandra02@gmail.com>
2023-02-13 16:30:53 -0800

    rename file and soln

--- Interviews/Cisco/max_drop_pts.py
+++ Interviews/Cisco/max_drop_pts.py
@@ -21,7 +21,7 @@ A path is valid path if, more than one drop points are connected
 (Single coordinate don't create any path, so pilot cannot fly over it).
 
 Constraints:
-1 < N, M 5 700 (where N is always equal to M)
+1 < N, M <= 700 (where N is always equal to M)
 
 Example:
 
@@ -38,3 +38,44 @@ The best path is the horizontal one covering (2,2), (2,6) and (2,8).
 So, the output is 3.
 
 """
+
+from collections import defaultdict
+
+
+def funcDrop(xCoordinate, yCoordinate):
+    # We will get the list of x an y coordinates here
+
+    x_points = defaultdict(int)
+    y_points = defaultdict(int)
+
+    # count the number of occurrences of each x and y coordinate
+    for x in xCoordinate:
+        x_points[x] += 1
+    for y in yCoordinate:
+        y_points[y] += 1
+
+    # get the coordinate with the maximum number of occurrences
+    max_x = max(x_points, key=x_points.get)
+    max_y = max(y_points, key=y_points.get)
+
+    # get the number of points in the best path
+    return max(x_points[max_x], y_points[max_y])
+
+
+def main():
+    # input for Coordinate
+    xCoordinate = []
+    xCoordinate_size = int(input())
+    xCoordinate = list(map(int, input().split()))
+
+    # input for Coordinate
+    yCoordinate = []
+    yCoordinate_size = int(input())
+    yCoordinate = list(map(int, input().split()))
+
+    result = funcDrop(xCoordinate, yCoordinate)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
