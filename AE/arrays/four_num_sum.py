813e84e6af343e1e1f9a94ce2c61cba2b02eefba
Surya-Dev <aaravchandra02@gmail.com>
2023-02-24 17:09:28 -0800

    soln (arrays) - four sum

--- AE/arrays/four_num_sum.py
+++ AE/arrays/four_num_sum.py
@@ -13,6 +13,35 @@
     targetSum = 16
 
   Sample Output:
-
+    [[7, 6, 4, -1], [7, 6, 1, 2]] 
 
 """
+
+# Avg - O(n^2) time | O(n^2) space
+# Worst - O(n^3) time | O(n^2) space
+
+
+def fourNumberSum(array, targetSum):
+    # dict to store sum ('q')
+    allPairSums = {}
+    resultQuadruplets = []
+
+    # Skipping as 1st and final iteration doesn't do anything as discussed in conceptual overview
+    # 1st iteration  - no value in the hash table is present.
+    # Last iteration - there are no values after it to sum with it.
+    for i in range(1, len(array) - 1):
+        for j in range(i + 1, len(array)):
+            currSum = array[i] + array[j]
+            compliment = targetSum - currSum
+            if compliment in allPairSums:
+                # adding 'p'
+                for pair in allPairSums[compliment]:
+                    resultQuadruplets.append(pair + [array[i], array[j]])
+        # find 'q' by iterating over each element before jth idx and creating pair.
+        for k in range(0, i):
+            currSum = array[i] + array[k]
+            if currSum not in allPairSums:
+                allPairSums[currSum] = [[array[i], array[k]]]
+            else:
+                allPairSums[currSum].append([array[i], array[k]])
+    return resultQuadruplets
