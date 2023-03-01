fed36badbc5c9a20b9c221f8e54c5bce69235cfa
Surya-Dev <aaravchandra02@gmail.com>
2023-03-01 10:12:13 -0800

    add sample inp/out

--- AE/searching/search_sorted_matrix.py
+++ AE/searching/search_sorted_matrix.py
@@ -1,4 +1,20 @@
 """
+
+Sample Input:
+
+    matrix = 
+    [
+    [1, 4, 7, 12, 15, 1000],
+    [2, 5, 19, 31, 32, 1001],
+    [3, 8, 24, 33, 35, 1002],
+    [40, 41, 42, 44, 45, 1003],
+    [99, 100, 103, 106, 128, 1004]
+    ]
+
+    target = 44
+
+Sample Output:
+    [3,3]
 """
 
 # O(m*n) time where m and n are dimensions of the matrix| O(1) space
