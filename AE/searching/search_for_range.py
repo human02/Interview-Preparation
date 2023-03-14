647b06ceecd9dbcec8cc897ba80c89f012c721dd
Surya-Dev <aaravchandra02@gmail.com>
2023-03-14 16:26:59 -0700

    soln (searching) - search for range

--- AE/searching/search_for_range.py
+++ AE/searching/search_for_range.py
@@ -17,3 +17,43 @@ Sample Output:
     [4, 9]
 
 """
+
+
+# O(log(n)) time | O(n) space due to recursive stack
+def searchForRange(array, target):
+    # Checking if target not found
+    finalRange = [-1, -1]
+    # for checking left extreme case
+    alteredBS(array, target, 0, len(array) - 1, finalRange, True)
+    alteredBS(array, target, 0, len(array) - 1, finalRange, False)
+    return finalRange
+
+
+def alteredBS(array, target, left, right, finalRange, goLeft):
+    while left <= right:
+        mid = (left + right) // 2
+        if array[mid] < target:
+            left = mid + 1
+        elif array[mid] > target:
+            right = mid - 1
+        # case where we found the target element
+        else:
+            # checking if its either extreme elems of target value
+            # check left case:
+            if goLeft:
+                # its the last target value on the left
+                if mid == 0 or array[mid - 1] != target:
+                    finalRange[0] = mid
+                    return
+                # more target values to the left therefore iterate left
+                else:
+                    right = mid - 1
+            # check right case:
+            else:
+                # its the last target value on the right
+                if mid == len(array) - 1 or array[mid + 1] != target:
+                    finalRange[1] = mid
+                    return
+                # more target values to the right therefore iterate right
+                else:
+                    left = mid + 1
