2860bb5c963f08bbcd7099985221fdddbb422c24
Surya-Dev <aaravchandra02@gmail.com>
2023-03-21 23:32:06 -0700

    soln (arrays) -zig zag traversal

--- AE/arrays/zigzag_traversal.py
+++ AE/arrays/zigzag_traversal.py
@@ -20,3 +20,56 @@
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
 """
+
+
+# O(n) time | O(n) complexity
+def zigzagTraverse(array):
+    # rows
+    height = len(array) - 1
+    # columns
+    width = len(array[0]) - 1
+    result = []
+
+    currRow = 0
+    currCol = 0
+    # direction tracking
+    goingDown = True
+
+    while not isOutOfBounds(currRow, currCol, height, width):
+        # append current value
+        result.append(array[currRow][currCol])
+
+        # if goingDown
+        if goingDown:
+            # turning case - last row or first col
+            if currRow == height or currCol == 0:
+                goingDown = False
+                # last row
+                if currRow == height:
+                    currCol += 1
+                # last column
+                else:
+                    currRow += 1
+            # if its normal
+            else:
+                currCol -= 1
+                currRow += 1
+
+        # if goingUp
+        else:
+            # turning case - last col or first row
+            if currRow == 0 or currCol == width:
+                goingDown = True
+                if currCol == width:
+                    currRow += 1
+                else:
+                    currCol += 1
+            # normal
+            else:
+                currRow -= 1
+                currCol += 1
+    return result
+
+
+def isOutOfBounds(row, col, height, width):
+    return row < 0 or row > height or col < 0 or col > width
