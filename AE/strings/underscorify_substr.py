71664813e44a9123a70ad85642e1553f294c387e
Surya-Dev <aaravchandra02@gmail.com>
2023-04-04 23:03:56 -0700

    soln (strings) - underscorify_substring

--- AE/strings/underscorify_substr.py
+++ AE/strings/underscorify_substr.py
@@ -18,3 +18,69 @@
     "_test_this is a _testtest_ to see if _testestest_ it works"
 
 """
+
+
+def underscorifySubstring(string, substring):
+    locations = collapse(getLocation(string, substring))
+    return underscorify(string, locations)
+
+
+def getLocation(string, substring):
+    locations = []
+    startIdx = 0
+    while startIdx < len(string):
+        nextIdx = string.find(substring, startIdx)
+        # meaning match found
+        if nextIdx != -1:
+            locations.append([nextIdx, nextIdx + len(substring)])
+            startIdx += 1
+        # as if no match then further cases wont also have any match
+        else:
+            break
+    return locations
+
+
+def collapse(locations):
+    if not locations:
+        return locations
+    collapsedLocations = [locations[0]]
+    prev = collapsedLocations[0]
+    for i in range(1, len(locations)):
+        curr = locations[i]
+        # overlap case
+        if curr[0] <= prev[1]:
+            prev[1] = curr[1]
+        else:
+            collapsedLocations.append(curr)
+            prev = curr
+    return collapsedLocations
+
+
+def underscorify(string, locations):
+    locationIdx = 0
+    stringIdx = 0
+    inBtwn = False
+    finalResult = []
+    i = 0  # counter used for checking both idxes of a location entry
+    while stringIdx < len(string) and locationIdx < len(locations):
+        if stringIdx == locations[locationIdx][i]:
+            finalResult.append("_")
+            inBtwn = not inBtwn
+            # increment inBtwn
+            if not inBtwn:
+                locationIdx += 1
+            # keep changing it to check both strt n end idx of an entry
+            i = 0 if i == 1 else 1
+        finalResult.append(string[stringIdx])
+        stringIdx += 1
+    # case for last '_'
+    if locationIdx < len(locations):
+        finalResult.append("_")
+    # add rest parts of the original string
+    elif stringIdx < len(string):
+        finalResult.append(string[stringIdx:])
+    # convert list to string
+    return "".join(finalResult)
+
+
+underscorifySubstring("testthis is a testtest to see if testestest it works", "test")
