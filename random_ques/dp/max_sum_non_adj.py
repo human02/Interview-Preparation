0a8083e49ad1252087ab4034b771a4a51bf6c11a
human02 <42168454+human02@users.noreply.github.com>
2025-09-09 00:09:52 -0700

    add soln

--- random_ques/dp/max_sum_non_adj.py
+++ random_ques/dp/max_sum_non_adj.py
@@ -9,3 +9,30 @@ Constraints:
 1 <= n <= 10^5
 0 <= nums[i] <= 10^4    
 """
+
+class Solution:
+    """ Function to calculate the maximum
+    sum of nonAdjacent elements """
+    def nonAdjacent(self, nums):
+        n = len(nums)
+        dp = [0] * n
+        
+        # Base case
+        dp[0] = nums[0]
+
+        # Iterate through the elements of the array
+        for i in range(1, n):
+            
+            """ Calculate maximum value by either picking
+            the current element or not picking it"""
+            pick = nums[i]
+            if i > 1:
+                pick += dp[i - 2]
+            nonPick = dp[i - 1]
+
+            # Store the maximum value in dp array
+            dp[i] = max(pick, nonPick)
+
+        """ The last element of the dp array
+        will contain the maximum sum"""
+        return dp[-1]
