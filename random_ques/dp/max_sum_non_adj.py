5cfe09787f1142dbfe4cd4fa4a0b7ed70e10d32d
human02 <42168454+human02@users.noreply.github.com>
2025-09-08 23:31:13 -0700

    add ques

---
+++ random_ques/dp/max_sum_non_adj.py
@@ -0,0 +1,11 @@
+"""
+Given an integer array nums of size n. 
+Return the maximum sum possible using the elements of nums such that no two elements taken are adjacent in nums.
+
+Example:
+Input: nums = [2,1,4,9]
+Output: 11   
+Constraints:
+1 <= n <= 10^5
+0 <= nums[i] <= 10^4    
+"""
