c68878aed1f640bc4199048b1782ef43dfa47e92
human02 <42168454+human02@users.noreply.github.com>
2025-09-12 11:45:20 -0700

    add memoized solution

--- random_ques/dp/grid_paths.py
+++ random_ques/dp/grid_paths.py
@@ -24,14 +24,32 @@ Constraints:
 1 <= n, m <= 100
 The answer will not exceed 109
 """
-def soln(m,n):
-    grid_paths_recur(m-1,n-1)
 
-def grid_paths_recur(i,j):
-    if i==0 or j==0:
+def soln_recur(m, n):
+    grid_paths_recur(m - 1, n - 1)
+
+def grid_paths_recur(i, j):
+    if i == 0 or j == 0:
         return 1
-    if i<0 or j<0:
+    if i < 0 or j < 0:
         return 0
-    up=grid_paths_recur(i-1,j)
-    left=grid_paths_recur(i,j-1)
-    return up+left
+    up = grid_paths_recur(i - 1, j)
+    left = grid_paths_recur(i, j - 1)
+    return up + left
+
+
+def soln_memo(m, n):
+    dp = [[-1 for j in range(n)] for i in range(m)]
+    grid_paths_memo(m - 1, n - 1, dp)
+
+def grid_paths_memo(i, j, dp):
+    if i == 0 or j == 0:
+        return 1
+    if i < 0 or j < 0:
+        return 0
+    if dp[i][j] != -1:
+        return dp[i][j]
+    up = grid_paths_memo(i - 1, j)
+    left = grid_paths_memo(i, j - 1)
+    dp[i][j] = up + left
+    return dp[i][j]
