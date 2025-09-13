251f4e8c59399d7c76bcc4587439e818f88b5343
human02 <42168454+human02@users.noreply.github.com>
2025-09-13 11:31:26 -0700

    add space optimized soln

--- random_ques/dp/grid_paths.py
+++ random_ques/dp/grid_paths.py
@@ -76,3 +76,37 @@ def grid_path_tabulation(m, n, dp):
                 left = dp(i, j - 1)
             dp[i][j] = up + left
         return dp[m - 1][n - 1]
+
+
+def grid_path_spaceOP(m, n):
+    # Function to solve the problem using space optimization
+    """Create a list to represent the previous row of the grid"""
+    prev = [0] * n
+
+    # Iterate through the rows of the grid
+    for i in range(m):
+        """Create a temporary list to represent the current row"""
+        temp = [0] * n
+
+        for j in range(n):
+            # Base case
+            if i == 0 and j == 0:
+                temp[j] = 1
+                continue
+
+            """ Initialize variables to store the number 
+            of ways from the cell above (up) and left (left)"""
+            up = prev[j] if i > 0 else 0
+            left = temp[j - 1] if j > 0 else 0
+
+            """ Calculate the number of ways to reach 
+            the current cell by adding 'up' and 'left'"""
+            temp[j] = up + left
+
+        """ Update the previous list with 
+        values calculated for the current row"""
+        prev = temp
+
+    """ The result is stored in the last
+    cell of the previous row (n-1)"""
+    return prev[-1]
