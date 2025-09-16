475b48291f762def31a78141739be306d6bf9e64
human02 <42168454+human02@users.noreply.github.com>
2025-09-15 20:58:02 -0700

    add ques

---
+++ random_ques/dp/longst_common_subsequence.py
@@ -0,0 +1,16 @@
+"""
+Longest common subsequence
+Given two strings str1 and str2, find the length of their longest common subsequence.
+A subsequence is a sequence that appears in the same relative order but not necessarily
+contiguous and a common subsequence of two strings is a subsequence that is common to both strings.
+
+Examples:
+Input: str1 = "bdefg", str2 = "bfg"
+Output: 3
+Explanation: The longest common subsequence is "bfg", which has a length of 3.
+
+Input: str1 = "mnop", str2 = "mnq"
+Output: 2
+Explanation: The longest common subsequence is "mn", which has a length of 2.
+
+"""
