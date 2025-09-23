ec4c945ef9eeb6ff661b945918eb3ee7ccf9ce5c
human02 <42168454+human02@users.noreply.github.com>
2025-09-23 08:46:17 -0700

    add ques

---
+++ random_ques/dp/coin_change.py
@@ -0,0 +1,20 @@
+"""
+You are given an integer array coins representing coins of different denominations (e.g. 1 dollar, 5 dollars, etc)
+and an integer amount representing a target amount of money.
+
+Return the fewest number of coins that you need to make up the exact target amount.
+If it is impossible to make up the amount, return -1.
+
+Example 1:
+Input: coins = [1,5,10], amount = 12
+Output: 3
+
+Example 2:
+Input: coins = [2], amount = 3
+Output: -1
+
+Constraints:
+1 <= coins.length <= 12
+1 <= coins[i] <= 2^31 - 1
+0 <= amount <= 10^4
+"""
