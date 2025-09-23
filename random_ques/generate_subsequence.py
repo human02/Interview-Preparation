2b8450d02c9d767817d6e4a02912556c3a0be491
human02 <42168454+human02@users.noreply.github.com>
2025-09-22 20:36:37 -0700

    add ques

---
+++ random_ques/generate_subsequence.py
@@ -0,0 +1,19 @@
+"""
+Generate Subsequences of a String
+
+Given a string s, return all possible non-empty subsequences of s, sorted in lexicographical order.
+A subsequence of a string is a new string generated from the original string with some characters (possibly none)
+deleted without changing the relative order of the remaining characters.
+
+Example 1:
+Input: s = "ba"
+Output: ["a","ab","b"]
+
+Example 2:
+Input: s = "xyz"
+Output: ["x","xy","xyz","xz","y","yz","z"]
+
+Constraints:
+1 <= s.length <= 16
+s consists of lowercase English letters.
+"""
