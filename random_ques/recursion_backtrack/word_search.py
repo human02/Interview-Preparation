07bec9d0b7beb4b3fed36a61d837204d62ce004d
human02 <42168454+human02@users.noreply.github.com>
2025-09-24 20:36:57 -0700

    add ques

---
+++ random_ques/recursion_backtrack/word_search.py
@@ -0,0 +1,18 @@
+"""
+Word Search
+
+Given a grid of n x m dimension grid of characters board and a string word.
+The word can be created by assembling the letters of successively surrounding cells,
+whether they are next to each other vertically or horizontally.
+It is forbidden to use the same letter cell more than once.
+
+Return true if the word exists in the grid otherwise false.
+
+Example 1:
+board = [ ["A", "B", "C", "E"] , ["S" ,"F" ,"C" ,"S"] , ["A", "D", "E", "E"] ] , word = "ABCCED"
+Output: true
+
+Example 2:
+board = [ ["A", "B", "C", "E"] , ["S" ,"F" ,"C" ,"S"] , ["A", "D", "E", "E"] ] , word = "SEE"
+Output: true
+"""
