42173614c12eea2a09806b8107745cd776337038
human02 <42168454+human02@users.noreply.github.com>
2025-09-24 20:37:13 -0700

    add soln

--- random_ques/recursion_backtrack/word_search.py
+++ random_ques/recursion_backtrack/word_search.py
@@ -16,3 +16,36 @@ Example 2:
 board = [ ["A", "B", "C", "E"] , ["S" ,"F" ,"C" ,"S"] , ["A", "D", "E", "E"] ] , word = "SEE"
 Output: true
 """
+
+
+class Solution:
+    def exist(self, board, word):
+        # your code goes here
+        rows = len(board)
+        cols = len(board[0])
+
+        def helper(i, j, w_idx):
+            # Base Cases
+            if w_idx == len(word):
+                return True
+            if i < 0 or j < 0 or i >= rows or j >= cols or word[w_idx] != board[i][j]:
+                return False
+
+            # Back Tracking Mechanism
+            tmp = board[i][j]
+            board[i][j] = "#"
+            res = (
+                helper(i + 1, j, w_idx + 1)
+                or helper(i, j + 1, w_idx + 1)
+                or helper(i - 1, j, w_idx + 1)
+                or helper(i, j - 1, w_idx + 1)
+            )
+            board[i][j] = tmp
+            return res
+
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == word[0]:
+                    if helper(i, j, 0):
+                        return True
+        return False
