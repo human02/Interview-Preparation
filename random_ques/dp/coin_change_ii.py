399572d0bc54076a105f4c1d0b0af5a84c4dc7d3
human02 <42168454+human02@users.noreply.github.com>
2025-09-26 23:45:53 -0700

    add tabu soln

--- random_ques/dp/coin_change_ii.py
+++ random_ques/dp/coin_change_ii.py
@@ -62,3 +62,20 @@ class Solution:
             return dp[(i, target)]
 
         return helper(0, amount) % (10**9 + 7)
+
+    def change_tab(self, coins, amount):
+        n = len(coins)
+        dp = [[0] * (amount + 1) for _ in range(n + 1)]
+
+        # base case: there's one way to make amount 0 (by choosing no coins)
+        for i in range(n + 1):
+            dp[i][0] = 1
+
+        for i in range(1, n + 1):
+            for j in range(amount + 1):
+                if coins[i - 1] <= j:
+                    dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j]
+
+        return dp[n][amount] % (10**9 + 7)
