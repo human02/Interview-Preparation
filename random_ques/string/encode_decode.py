ecd84bf8dda7c0e75eb3da987c9a4448f0b2cc7f
human02 <42168454+human02@users.noreply.github.com>
2025-09-28 12:41:55 -0700

    add soln

--- random_ques/string/encode_decode.py
+++ random_ques/string/encode_decode.py
@@ -1,5 +1,6 @@
 """
-Design an algorithm to encode a list of strings to a single string. The encoded string is then decoded back to the original list of strings.
+Design an algorithm to encode a list of strings to a single string.
+The encoded string is then decoded back to the original list of strings.
 
 Please implement encode and decode
 
@@ -11,3 +12,24 @@ Example 2:
 Input: ["we","say",":","yes"]
 Output: ["we","say",":","yes"]
 """
+
+
+class Solution:
+    def encode(self, strs):
+        s = ""
+        for wrd in strs:
+            s += str(len(wrd)) + "#" + wrd
+        return s
+
+    def decode(self, s):
+        i = 0
+        res = []
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(s[i:j])
+            start = j + 1
+            res.append(s[start : start + length])
+            i = start + length
+        return res
