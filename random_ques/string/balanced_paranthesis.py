f54791d2d5ab4cce59f4c3686611fc9c9affc4a0
human02 <42168454+human02@users.noreply.github.com>
2025-09-27 23:50:18 -0700

    add soln with comments

--- random_ques/string/balanced_paranthesis.py
+++ random_ques/string/balanced_paranthesis.py
@@ -9,3 +9,37 @@ Input: str = “[()”
 Output: False
 Explanation: As ‘[‘ does not have ‘]’ hence it is not valid and will return false.
 """
+
+
+class Solution:
+    def isValid(self, str: str) -> bool:
+        # stack to store brackets
+        st = []
+
+        # Inner function to check brackets
+        def isSameType(opn, clsd):
+            if (
+                (opn == "(" and clsd == ")")
+                or (opn == "{" and clsd == "}")
+                or (opn == "[" and clsd == "]")
+            ):
+                return True
+            return False
+
+        for i in str:
+            # push only open brackets to Stack
+            if i == "(" or i == "{" or i == "[":
+                st.append(i)
+            else:
+                # check stack, if empty = open brackets over, hence False
+                if not st:
+                    return False
+                # check for bracket type mismatch
+                if not isSameType(st.pop(), i):
+                    return False
+
+        # String traversed, but open brackets remaining in stack
+        if st:
+            return False
+        # All hurdles passed!
+        return True
