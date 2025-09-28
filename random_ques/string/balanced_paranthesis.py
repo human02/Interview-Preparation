5011522510b96fae79fbb32a1649a8cd5dbf8bb9
human02 <42168454+human02@users.noreply.github.com>
2025-09-27 23:38:28 -0700

    add ques

---
+++ random_ques/string/balanced_paranthesis.py
@@ -0,0 +1,11 @@
+"""
+Given string str containing just the characters '(', ')', '{', '}', '[' and ']', check if the input string is valid and return true if the string is balanced otherwise return false.
+
+Input: str = “()[{}()]”
+Output: True
+Explanation: As every open bracket has its corresponding close bracket. Match parentheses are in correct order hence they are balanced.
+
+Input: str = “[()”
+Output: False
+Explanation: As ‘[‘ does not have ‘]’ hence it is not valid and will return false.
+"""
