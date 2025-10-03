37b5442884a9108687c9e1abe543198ac7139d30
human02 <42168454+human02@users.noreply.github.com>
2025-10-03 01:11:18 -0700

    add ques

---
+++ random_ques/min_races.py
@@ -0,0 +1,6 @@
+"""
+Find the Fastest 3 Horses
+
+You have 25 horses and a race track that can accommodate only 5 horses at a time. Each horse has a consistent speed, but you don't have a stopwatch to measure their actual speeds. You can only determine the relative order of horses within each race.
+Your task: Write a function that finds the top 3 fastest horses using the minimum number of races.
+"""
