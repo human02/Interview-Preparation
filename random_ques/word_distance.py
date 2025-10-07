25b71cc19084c2897a6c2bf39d48e6da0c77b34f
human02 <42168454+human02@users.noreply.github.com>
2025-10-07 13:11:41 -0700

    add ques

---
+++ random_ques/word_distance.py
@@ -0,0 +1,4 @@
+"""
+Given a page from the book, and 2 word that might or might not be in this book. Find the minimum distance between the two words.
+cat, dog, bookpage="the dog is the house of the cat cat and is the dog awesome cat"
+"""
