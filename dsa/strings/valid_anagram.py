af6d08c9da5ba93073a7268fd97edda0079c08b6
human02 <42168454+human02@users.noreply.github.com>
2025-10-09 14:28:01 -0700

    add soln

--- dsa/strings/valid_anagram.py
+++ dsa/strings/valid_anagram.py
@@ -19,3 +19,25 @@ Output: false
 Constraints:
 s and t doesn't only consist of lowercase English letters.
 """
+
+
+# TC - O(n+n) = O(n)
+# SC - O(n)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # not anagram is not same length
+        if len(s) != len(t):
+            return False
+
+        # Using freq add and subtract method
+        freq = {}
+
+        for ch in s:
+            freq[ch] = freq.get(ch, 0) + 1
+
+        for ch in t:
+            freq[ch] = freq.get(ch, 0) - 1
+            if freq[ch] < 0:
+                return False
+
+        return all(count == 0 for count in freq.values())
