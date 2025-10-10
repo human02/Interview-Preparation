9a9a67b765192c6ab74e7c98e0ee387075bd891b
human02 <42168454+human02@users.noreply.github.com>
2025-10-10 00:10:20 -0700

    add ans

--- dsa/arrays/diagonal_traverse.py
+++ dsa/arrays/diagonal_traverse.py
@@ -17,3 +17,38 @@ n == mat[i].length
 1 <= m * n <= 104
 -105 <= mat[i][j] <= 105
 """
+
+
+class Solution:
+    def traverse_diag(self, matrix):
+        if not matrix:
+            return []
+
+        result = []
+
+        rows, cols = len(matrix), len(matrix[0])
+
+        # d = number of diagonals => Total Diagonals = rows+cols-1
+        for d in range(rows + cols - 1):
+            intermediate = []
+
+            # Calculate starting point for each diagonal
+            if d < cols:
+                row = 0
+                col = d
+            else:
+                row = d - cols + 1
+                col = cols - 1
+
+            # top right to bottom left travesrsal of diagonal
+            while r < rows and c >= 0:
+                intermediate.append(matrix[r][c])
+                r += 1
+                c -= 1
+
+            # Reversing for zigzag
+            if d % 2 == 0:
+                result.extend(intermediate[::-1])
+            else:
+                result.extend(intermediate)
+        return result
