8530f1f21ca89d8bee9ccf74c1029f12515d5da6
human02 <42168454+human02@users.noreply.github.com>
2025-10-20 11:46:15 -0700

    add ques

---
+++ dsa/stack_queues/trapping_rainwater.py
@@ -0,0 +1,24 @@
+"""
+Trapping Rainwater
+
+Given an array of non-negative integers, height representing the elevation of ground.
+Calculate the amount of water that can be trapped after rain.
+
+
+Examples:
+Input: height= [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+Output: 6
+Explanation: As seen from the diagram 1+1+2+1+1=6 unit of water can be trapped
+
+Input: height= [4, 2, 0, 3, 2, 5]
+Output: 9
+Expalanation: 2+4+1+2=9 unit of water can be trapped
+
+Input: height= [7, 4, 0, 9]
+Output:10
+
+Constraints:
+  n == height.length
+  1 <= n <= 105
+  0 <= height[i] <= 105
+"""
