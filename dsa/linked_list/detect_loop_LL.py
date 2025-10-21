48ea6ad7a1ed8bafa47f0f939dcfa4e3c3da0876
human02 <42168454+human02@users.noreply.github.com>
2025-10-21 01:03:41 -0700

    add ans

--- dsa/linked_list/detect_loop_LL.py
+++ dsa/linked_list/detect_loop_LL.py
@@ -23,3 +23,14 @@ Constraints:
 0 <= ListNode.val <= 104
 pos is -1 or a valid index in the linked list
 """
+
+
+class Solution:
+    def find_cycle(self, head):
+        slow, fast = head, head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
