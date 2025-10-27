90b957caa5e72c9ba8e22874ce7cf96d04c55cf9
human02 <42168454+human02@users.noreply.github.com>
2025-10-27 13:30:36 -0700

    add soln

--- dsa/binary_search/aggressive_cows.py
+++ dsa/binary_search/aggressive_cows.py
@@ -25,3 +25,50 @@ Constraints:
   0 <= nums[i] <= 109
 
 """
+
+
+class Solution:
+    def isPossible(self, gap, nums, k):
+        c = 1
+        n = len(nums)
+        last = nums[0]
+        for i in range(1, n):
+            if nums[i] - last >= gap:
+                c += 1
+                last = nums[i]
+        return c >= k
+
+    def find_distance_brute(self, nums, k):
+        n = len(nums)
+        nums.sort()
+        limit = nums[-1] - nums[0]
+        ans = 0
+        for i in range(1, limit + 1):
+            if self.isPossible(i, nums, k):
+                ans = i
+        return ans
+
+    def find_distance_optimal(self, nums, k):
+        n = len(nums)
+        low = 1
+        nums.sort()
+        high = nums[-1] - nums[0]
+        ans = 0
+        while low <= high:
+            mid = (low + high) // 2
+            if self.isPossible(mid, nums, k):
+                ans = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+        return ans
+
+
+if __name__ == "__main__":
+    obj = Solution()
+    print(obj.find_distance_brute([0, 3, 4, 7, 10, 9], 4))
+    print(obj.find_distance_optimal([0, 3, 4, 7, 10, 9], 4))
+    print(obj.find_distance_brute([4, 2, 1, 3, 6], 2))
+    print(obj.find_distance_optimal([4, 2, 1, 3, 6], 2))
+    print(obj.find_distance_brute([10, 1, 2, 7, 5], 3))
+    print(obj.find_distance_optimal([10, 1, 2, 7, 5], 3))
