5cd8415875ea965fe7834712725f4b880e32cc42
human02 <42168454+human02@users.noreply.github.com>
2025-10-30 12:48:10 -0700

    add ques

---
+++ dsa/stack_queues/largest_rectangle.py
@@ -0,0 +1,23 @@
+"""
+Largest rectangle in a histogram
+
+Given an array of integers heights representing the histogram's bar height where the width of each bar is 1
+return the area of the largest rectangle in the histogram.
+
+Examples:
+Input: heights = [2, 1, 5, 6, 2, 3]
+Output: 10
+Explanation: The largest rectangle is highlighted, which has an area of 5*2 = 10 units.
+
+Input: heights = [3, 5, 1, 7, 5, 9]
+Output: 15
+Explanation: The largest rectangle has an area of 5*3 = 15 units.
+
+Input: heights = [2,4]
+Output: 4
+
+Constraints:
+  1 <= heights.length <= 105
+  0 <= heights[i] <= 104
+
+"""
