6cb37521af2d6c9318a2f4abb67f951c62c13d68
human02 <42168454+human02@users.noreply.github.com>
2025-10-30 12:48:31 -0700

    add soln

--- dsa/stack_queues/largest_rectangle.py
+++ dsa/stack_queues/largest_rectangle.py
@@ -21,3 +21,74 @@ Constraints:
   0 <= heights[i] <= 104
 
 """
+
+
+class Solution:
+    """
+    Idea is to find next smallest and prev smallest for each index.
+    Because the smaller limit restricts the rectangle.
+    """
+
+    def findNSE(self, arr):
+        n = len(arr)
+        ans = [0] * n
+        st = []
+
+        for i in range(n - 1, -1, -1):
+            # Pop the elements in the stack until the stack is not empty and the top element is not the smaller element
+            while st and arr[st[-1]] >= arr[i]:
+                st.pop()
+
+            # Update the answer
+            if st:
+                ans[i] = st[-1]
+            else:
+                ans[i] = n
+
+            # Push the index of current element in the stack
+            st.append(i)
+        return ans
+
+    # Function to find the indices of previous smaller elements
+    def findPSE(self, arr):
+        n = len(arr)
+        ans = [0] * n
+        st = []
+
+        for i in range(n):
+            # Pop the elements in the stack until the stack is not empty and the top elements is not the smaller element
+            while st and arr[st[-1]] >= arr[i]:
+                st.pop()
+
+            # Update the answer
+            if st:
+                ans[i] = st[-1]
+            else:
+                ans[i] = n
+
+            # Push the index of current element in the stack
+            st.append(i)
+        return ans
+
+    # Function to find the largest rectangle area
+    def largestRectangleArea(self, heights):
+        nse = self.findNSE(heights)
+        pse = self.findPSE(heights)
+
+        largestArea = 0
+        area = 0
+
+        for i in range(len(heights)):
+            # Calculate current area
+            area = heights[i] * (nse[i] - pse[i] - 1)
+            largestArea = max(largestArea, area)
+        return largestArea
+
+
+# Example usage
+if __name__ == "__main__":
+    heights = [2, 1, 5, 6, 2, 3]
+    sol = Solution()
+    # Function call to find the largest rectangle area
+    ans = sol.largestRectangleArea(heights)
+    print("The largest rectangle area is:", ans)
