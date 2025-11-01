7f8c619c9931695831f8355dfa193c7708d52a8f
human02 <42168454+human02@users.noreply.github.com>
2025-11-01 15:38:02 -0700

    add ques

---
+++ dsa/heap/implement_maxHeap.py
@@ -0,0 +1,13 @@
+"""
+Implement Max Heap
+
+You need to implement the Max Heap with the following given methods:
+
+insert (x) -> insert value x to the max heap
+getMax -> Output the maximum value from max heap
+extractMax -> Remove the maximum element from the heap
+heapSize -> return the current size of the heap
+isEmpty -> returns if heap is empty or not
+changeKey (ind, val) -> update the value at given index to val (index will be given 0-based indexing)
+initializeHeap -> Initialize the heap
+"""
