9846e322f0abf278f5d404811c8e57ddb2968266
human02 <42168454+human02@users.noreply.github.com>
2025-11-02 13:10:28 -0800

    add ques

---
+++ dsa/graph/rotten_oranges.py
@@ -0,0 +1,27 @@
+"""
+Rotten Oranges
+
+Given an n x m grid, where each cell has the following values :
+2 - represents a rotten orange
+1 - represents a Fresh orange
+0 - represents an Empty Cell
+
+Every minute, if a fresh orange is adjacent to a rotten orange in 4-direction, it becomes rotten.
+Return the minimum number of minutes required such that none of the cells has a Fresh Orange.
+If it's not possible, return -1.
+
+Examples:
+Input: grid = [ [2, 1, 1] , [0, 1, 1] , [1, 0, 1] ]
+Output: -1
+Explanation: Orange at (3,0) cannot be rotten.
+
+Input: grid = [ [2,1,1] , [1,1,0] , [0,1,1] ]
+Output: 4
+
+Input: grid = [[0,1,2],[0,1,2],[2,1,1]]
+Output: 1
+
+Constraints:
+  1 <= n, m <= 500
+  grid[i][j] == 0 or 1 or 2
+"""
