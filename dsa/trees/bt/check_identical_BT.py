53f53399cd4c53741e55d9709d8c066d89c90ce0
human02 <42168454+human02@users.noreply.github.com>
2025-11-05 11:06:15 -0800

    add ques

---
+++ dsa/trees/bt/check_identical_BT.py
@@ -0,0 +1,22 @@
+"""
+Check if two trees are identical or not
+
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+Examples:
+Input : p = [1, 2, 3] , q = [1, 2, 3]
+Output : true
+Explanation : Both trees images are shown below
+
+Input : p = [1, 2, 1] , q = [1, 1, 2]
+Output : false
+Explanation : Both trees images are shown below
+
+Input : p = [5, 1, 2, 8, null, null, 5, null, 4, null, null, 7 ], q = [5, 1, 2, 8, null, null, 4, null, 5, null, null, 7 ]
+Output: false
+
+Constraints:
+0 <= Number of Nodes <= 100
+-104 <= Node.val <= 104
+"""
