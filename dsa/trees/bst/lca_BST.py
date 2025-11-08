b8167b30496e7a3f33e425ebd881d3e3cd3d38be
human02 <42168454+human02@users.noreply.github.com>
2025-11-08 09:45:35 -0800

    add ques

---
+++ dsa/trees/bst/lca_BST.py
@@ -0,0 +1,17 @@
+"""
+LCA in BST
+
+Given the root node of a binary search tree (BST) and two node values p,q.
+Return the lowest common ancestors(LCA) of the two nodes in BST.
+
+Examples:
+Input : root = [5, 3, 6, 2, 4, null, 7] , p = 2, q = 4
+Output : [3]
+
+Input : root = [5, 3, 6, 2, 4, null, 7] , p = 2, q = 7
+Output : [5]
+
+Input : root = [2, 1, 4, null, null, 3, 6] , p = 1, q = 6
+Output : [2]
+
+"""
