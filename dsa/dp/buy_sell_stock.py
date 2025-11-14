d9fae3bb4df34ca812382f240e6ee32d64d73252
human02 <42168454+human02@users.noreply.github.com>
2025-11-14 08:17:09 -0800

    add ques

---
+++ dsa/dp/buy_sell_stock.py
@@ -0,0 +1,26 @@
+"""
+
+Best Time to Buy and Sell Stock
+
+Given an array arr of n integers, where arr[i] represents price of the stock on the ith day.
+Determine the maximum profit achievable by buying and selling the stock AT MOST ONCE.
+
+The stock should be purchased before selling it, and both actions cannot occur on the same day.
+
+Examples:
+Input: arr = [10, 7, 5, 8, 11, 9]
+Output: 6
+Explanation: Buy on day 3 (price = 5) and sell on day 5 (price = 11), profit = 11 - 5 = 6.
+
+Input: arr = [5, 4, 3, 2, 1]
+Output: 0
+Explanation: In this case, no transactions are made. Therefore, the maximum profit remains 0.
+
+Input: arr = [3, 8, 1, 4, 6, 2]
+Output: 5
+
+Constraints:
+    1 <= n<= 105
+    0 <= arr[i] <= 106
+
+"""
