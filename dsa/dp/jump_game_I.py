e6d001b8597addbd234ba0ee10386f2b8f35c934
human02 <42168454+human02@users.noreply.github.com>
2025-11-13 17:58:17 -0800

    add ques

---
+++ dsa/dp/jump_game_I.py
@@ -0,0 +1,30 @@
+"""
+
+Jump Game - I
+
+Given an array of integers nums, each element in the array represents the maximum jump length at that position.
+Initially starting at the first index of the array, determine if it is possible to reach the last index.
+Return true if the last index can be reached, otherwise return false.
+
+Input : [2, 3, 1, 1, 4]
+Output : true
+Explanation : We can simply take Jump of 1 step at each index to reach the last index.
+
+Input : [3, 2, 1, 0, 4]
+Output : false
+Explanation : No matter how you make jumps you will always reach the third index (0 base) of the array.
+The maximum jump of index three is 0, So you can never reach the last index of array.
+
+Input : [5, 3, 2, 1, 0]
+Output : true
+
+Constraints:
+1 <= nums.length <= 104
+0 <= nums[i] <= 105
+"""
+
+""" 
+We use greedy approach here. 
+We want to check at each index, what is the max reachabele index from that particular index.
+# If at any index the max reachable index is less than or equal to that index, we cannot reach the end.
+"""
