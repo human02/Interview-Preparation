8120edb03b015eea3ad05c2c2730678f4bee1db5
human02 <42168454+human02@users.noreply.github.com>
2025-11-15 19:30:21 -0800

    add soln

--- dsa/linked_list/segg_odd_even.py
+++ dsa/linked_list/segg_odd_even.py
@@ -22,3 +22,61 @@ Constraints:
     0 <= ListNode.val <= 104
 
 """
+
+
+class ListNode:
+    def __init__(self, data, next=None):
+        self.next = next
+        self.data = data
+
+
+class Solution:
+    # TC - O(2*(n/2)) = O(n), SC - O(1)
+    def seggOddEven(self, head):
+        if head is None or head.next is None:
+            return None
+        odd = head
+        even = head.next
+        firstEven = head.next
+
+        # need to reach the 2nd last even only
+        while even and even.next:
+            odd.next = odd.next.next
+            even.next = even.next.next
+
+            odd = odd.next
+            even = even.next
+
+        # Now put even list at the end of odd list
+        odd.next = firstEven
+        return head
+
+
+# Function to print the linked list
+def printLL(head):
+    while head:
+        print(head.data, end=" ")
+        head = head.next
+    print()
+
+
+if __name__ == "__main__":
+    obj = Solution()
+
+    # Create a linked list with given values
+    arr = [1, 3, 4, 2, 5, 6]
+    head = ListNode(arr[0])
+    head.next = ListNode(arr[1])
+    head.next.next = ListNode(arr[2])
+    head.next.next.next = ListNode(arr[3])
+    head.next.next.next.next = ListNode(arr[4])
+    head.next.next.next.next.next = ListNode(arr[5])
+
+    # Print the original linked list
+    print("Original Linked List: ", end="")
+    printLL(head)
+
+    # Rearrange the list and print it
+    print(obj.seggOddEven(head))
+    print("New Linked List: ", end="")
+    printLL(head)
