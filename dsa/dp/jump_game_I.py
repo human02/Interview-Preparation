1bdf3485b5d014cb64d6fe905526dad2d49404b9
human02 <42168454+human02@users.noreply.github.com>
2025-11-21 14:46:52 -0800

    edit comment

--- dsa/dp/jump_game_I.py
+++ dsa/dp/jump_game_I.py
@@ -7,26 +7,28 @@ Initially starting at the first index of the array, determine if it is possible 
 Return true if the last index can be reached, otherwise return false.
 
 Input : [2, 3, 1, 1, 4]
-Output : true
+Output : True
 Explanation : We can simply take Jump of 1 step at each index to reach the last index.
 
 Input : [3, 2, 1, 0, 4]
-Output : false
+Output : False
 Explanation : No matter how you make jumps you will always reach the third index (0 base) of the array.
 The maximum jump of index three is 0, So you can never reach the last index of array.
 
 Input : [5, 3, 2, 1, 0]
-Output : true
+Output : True
 
 Constraints:
-1 <= nums.length <= 104
-0 <= nums[i] <= 105
+    1 <= nums.length <= 104
+    0 <= nums[i] <= 105
+
 """
 
 """ 
 We use greedy approach here. 
 We want to check at each index, what is the max reachabele index from that particular index.
-# If at any index the max reachable index is less than or equal to that index, we cannot reach the end.
+If at any point we encounter an index that is beyond what we can reach (i > maxReach), 
+we cannot proceed further and thus cannot reach the end.
 """
 
 
