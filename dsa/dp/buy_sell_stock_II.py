ab7581906e3b94dbf43dd771f66b916bd99ccf47
human02 <42168454+human02@users.noreply.github.com>
2025-11-22 10:46:46 -0800

    add soln

--- dsa/dp/buy_sell_stock_II.py
+++ dsa/dp/buy_sell_stock_II.py
@@ -24,3 +24,84 @@ Constraints:
     0 <= arr[i] <= 106
 
 """
+
+
+class Solution:
+    # TC - O(2^n), SC - O(n)
+    def stockBuySell_recur(self, arr):
+        n = len(arr)
+
+        def helper(ind, buy):
+
+            if ind == n:
+                return 0  # no profit even if you hold stocks
+
+            profit = 0
+
+            # able to buy
+            if buy == 1:
+                # Profit = sell + "- buy"
+                # max(not buy today, buy today)
+                profit = max(
+                    0 + helper(ind + 1, 1),
+                    -arr[ind] + helper(ind + 1, 0),
+                )
+
+            # able to sell
+            else:
+                # Profit = "+sell" - buy
+                # max(not sell today,sell today)
+                profit = max(
+                    0 + helper(ind + 1, 0),
+                    arr[ind] + helper(ind + 1, 1),
+                )
+            return profit
+
+        return helper(0, 1)
+
+    # TC - O(n*2), SC - O(n*2) for dp + O(n) for stack
+    def stockBuySell_memo(self, arr):
+        n = len(arr)
+        dp = [[-1] * 2 for _ in range(n)]
+
+        def helper(ind, buy, dp):
+
+            if ind == n:
+                return 0  # no profit even if you hold stocks
+
+            if dp[ind][buy] != -1:
+                return dp[ind][buy]
+
+            profit = 0
+
+            # able to buy
+            if buy == 1:
+                # Profit = sell + "- buy"
+                # max(not buy today, buy today)
+                profit = max(
+                    0 + helper(ind + 1, 1, dp),
+                    -arr[ind] + helper(ind + 1, 0, dp),
+                )
+
+            # able to sell
+            else:
+                # Profit = "+sell" - buy
+                # max(not sell today,sell today)
+                profit = max(
+                    0 + helper(ind + 1, 0, dp),
+                    arr[ind] + helper(ind + 1, 1, dp),
+                )
+            dp[ind][buy] = profit
+            return dp[ind][buy]
+
+        return helper(0, 1, dp)
+
+
+if __name__ == "__main__":
+    obj = Solution()
+    print(f"\n Answer = {obj.stockBuySell_recur([10, 7, 5, 8, 11, 9],)}\n")
+    print(f"\n Answer = {obj.stockBuySell_memo([10, 7, 5, 8, 11, 9],)}\n")
+    print(f"\n Answer = {obj.stockBuySell_recur([5, 4, 3, 2, 1],)}\n")
+    print(f"\n Answer = {obj.stockBuySell_memo([5, 4, 3, 2, 1],)}\n")
+    print(f"\n Answer = {obj.stockBuySell_recur([3, 8, 1, 4, 6, 2],)}\n")
+    print(f"\n Answer = {obj.stockBuySell_memo([3, 8, 1, 4, 6, 2],)}\n")
