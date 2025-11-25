fe006f95045e698d6548a76bf03a5903c258f2a6
human02 <42168454+human02@users.noreply.github.com>
2025-11-25 14:43:19 -0800

    add soln

--- dsa/linked_list/sort_LL.py
+++ dsa/linked_list/sort_LL.py
@@ -23,3 +23,61 @@ Constraints:
     -104 <= ListNode.val <= 104
 
 """
+
+
+class ListNode:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    # TC - O(nlogn) + O(n) + O(n), SC - O(n) + O(n)
+    def sortLL_brute(self, head):
+        values = []
+
+        # take out all values from LL
+        tmp = head
+        while tmp:  # O(n)
+            values.append(tmp.val)
+            tmp = tmp.next
+
+        values.sort()  # O(nlogn)
+
+        dummyHead = ListNode(-1)
+        tmp = dummyHead
+        for value in values:  # O(n)
+            currNode = ListNode(value)
+            tmp.next = currNode
+            tmp = tmp.next
+
+        return dummyHead.next
+
+
+# Function to print the linked list
+def printLinkedList(head):
+    temp = head
+    while temp:
+        # Print the data of the current node
+        print(temp.val, end=" ")
+        temp = temp.next
+    print()
+
+
+if __name__ == "__main__":
+    obj = Solution()
+
+    # Linked List: 5 6 1 2 1
+    head = ListNode(5)
+    head.next = ListNode(6)
+    head.next.next = ListNode(1)
+    head.next.next.next = ListNode(2)
+    head.next.next.next.next = ListNode(1)
+
+    print("Original Linked List: ", end="")
+    printLinkedList(head)
+
+    head = obj.sortLL_brute(head)
+
+    print("Sorted Linked List: ", end="")
+    printLinkedList(head)
