500fb33c1e54e2837984b2c57018b713b6330e96
human02 <42168454+human02@users.noreply.github.com>
2025-11-27 13:26:30 -0800

    add soln space optmzd

--- dsa/dp/house_robber_I.py
+++ dsa/dp/house_robber_I.py
@@ -10,8 +10,6 @@ automatically contact the police if two adjacent houses were broken into on the 
 Given an integer array nums representing the amount of money of each house, return the maximum
 amount of money you can rob tonight without alerting the police.
 
-
-
 Example 1:
 Input: nums = [1,2,3,1]
 Output: 4
@@ -23,7 +21,6 @@ Input: nums = [2,7,9,3,1]
 Output: 12
 Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
 Total amount you can rob = 2 + 9 + 1 = 12.
-
 
 Constraints:
     1 <= nums.length <= 100
@@ -90,6 +87,22 @@ class Solution:
             dp[ind] = max(pick, not_pick)
         return dp[-1]
 
+    # TC - O(n), SC - O(1)
+    def robHouses_spaceOP(self, nums):
+        n = len(nums)
+        prev = nums[0]
+        prev2 = 0
+
+        for ind in range(1, n):
+            pick = nums[ind]
+            if ind > 1:
+                pick += prev2
+            not_pick = prev
+            curr_i = max(pick, not_pick)
+            prev2 = prev
+            prev = curr_i
+        return prev
+
 
 if __name__ == "__main__":
     obj = Solution()
@@ -101,3 +114,6 @@ if __name__ == "__main__":
     print()
     print(obj.robHouses_tabu([1, 2, 3, 1]))
     print(obj.robHouses_tabu([2, 7, 9, 3, 1]))
+    print()
+    print(obj.robHouses_spaceOP([1, 2, 3, 1]))
+    print(obj.robHouses_spaceOP([2, 7, 9, 3, 1]))
