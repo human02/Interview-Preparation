a130f0c337e9309e837d5e1bf7aaa3504f444ec6
human02 <42168454+human02@users.noreply.github.com>
2025-11-30 15:02:23 -0800

    add soln

--- dsa/arrays/max_prod_subarray.py
+++ dsa/arrays/max_prod_subarray.py
@@ -23,3 +23,72 @@ Constraints:
     -109 <= product of any prefix or suffix of nums <= 109
 
 """
+
+
+class Solution:
+    def maxProduct_brute(self, nums):
+        """
+        Finding out all subsequence and then picking the rest
+        """
+        maxi = float("-inf")
+        for i in range(len(nums)):
+            for j in range(i, len(nums)):
+                prod = 1
+                for k in range(i, j + 1):
+                    prod = nums[k] * prod
+                    maxi = max(maxi, prod)
+        return maxi
+
+    def maxProduct_better(self, nums):
+        """
+        Dont need 3 loop and can be done in 2 loops
+        """
+
+        maxi = float("-inf")
+        for i in range(len(nums)):
+            prod = 1
+            for j in range(i, len(nums)):
+                prod = nums[j] * prod
+                maxi = max(maxi, prod)
+        return maxi
+
+    # Function to find the product of elements in maximum product subarray
+    def maxProduct_optimal(self, nums):
+        """
+        Observation based:
+        - all (+)ve values
+        - even (-)ve values
+        - odd (-)ve values --> remove 1 (-)ve, use prefix and suffix variables
+        - Handle 0 case
+        """
+
+        n = len(nums)
+
+        ans = float("-inf")  # to store the answer
+
+        # Indices to store the prefix and suffix multiplication
+        prefix, suffix = 1, 1
+
+        for i in range(n):
+
+            if prefix == 0:
+                prefix = 1
+            if suffix == 0:
+                suffix = 1
+
+            # update the prefix and suffix multiplication
+            prefix *= nums[i]
+            suffix *= nums[n - i - 1]
+
+            # store the maximum as the answer
+            ans = max(ans, max(prefix, suffix))
+
+        return ans
+
+
+if __name__ == "__main__":
+    # 0  1  2  3  4  5
+    nums = [4, 5, 3, 7, 1, 2]
+    sol = Solution()
+    ans = sol.maxProduct_optimal(nums)
+    print("The product of elements in maximum product subarray is:", ans)
