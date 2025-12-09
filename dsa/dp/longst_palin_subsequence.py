f8ea45fe38c8a647a1ad7a615e288b5b621697a1
human02 <42168454+human02@users.noreply.github.com>
2025-12-09 12:07:49 -0800

    add ques

--- dsa/dp/longst_palin_subsequence.py
+++ dsa/dp/longst_palin_subsequence.py
@@ -1,1 +1,27 @@
+"""
 
+Longest Palindromic Subsequence
+
+Given a string, Find the longest palindromic subsequence length in given string.
+A palindrome is a sequence that reads the same backwards as forward.
+A subsequence is a sequence that can be derived from another sequence by deleting some or no elements
+without changing the order of the remaining elements.
+
+Example 1:
+Input: s = "eeeme"
+Output: 4
+Explanation: The longest palindromic subsequence is "eeee", which has a length of 4.
+
+Example 2:
+Input: s = "annb"
+Output: 2
+Explanation: The longest palindromic subsequence is "nn", which has a length of 2.
+
+Example 3:
+Input: s = "s"
+Output: 1
+
+Constraints:
+    1 ≤ s.length ≤ 1000
+
+"""
