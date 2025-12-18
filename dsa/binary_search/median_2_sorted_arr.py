7477fd26d921be4b41b744e5f4a909ac9d5f2a2f
human02 <42168454+human02@users.noreply.github.com>
2025-12-18 12:40:59 -0800

    add soln - brute

--- dsa/binary_search/median_2_sorted_arr.py
+++ dsa/binary_search/median_2_sorted_arr.py
@@ -25,3 +25,48 @@ Constraints:
     -106 <= nums1[i], nums2[i] <= 106
 
 """
+
+
+class Solution:
+    # TC - O(m+n), SC - O(m+n)
+    def findMedian_brute(self, nums1, nums2):
+        """
+        Idea:
+            - merge both arrays to a new array and find the median
+            - total even elements -> (nums[n//2]+nums[n//2-1])//2
+            - total odd elements -> (nums[n//2]+nums[n//2-1])//2
+        """
+
+        merged_nums = []
+        n1 = len(nums1)
+        n2 = len(nums2)
+        i, j = 0, 0
+
+        # Create new merged list
+        while i < n1 and j < n2:
+            if nums1[i] < nums2[j]:
+                merged_nums.append(nums1[i])
+                i += 1
+            else:
+                merged_nums.append(nums2[j])
+                j += 1
+
+        while i < n1:
+            merged_nums.append(nums1[i])
+            i += 1
+
+        while j < n2:
+            merged_nums.append(nums2[j])
+            j += 1
+
+        if len(merged_nums) % 2 == 0:
+            ele1 = merged_nums[len(merged_nums) // 2]
+            ele2 = merged_nums[(len(merged_nums) // 2) - 1]
+            return (ele1 + ele2) / 2
+        else:
+            return float(merged_nums[len(merged_nums) // 2])
+
+if __name__ == "__main__":
+    obj = Solution()
+    print(obj.findMedian_brute([1, 3], [2]))
+    print(obj.findMedian_brute([1, 2], [3, 4]))
