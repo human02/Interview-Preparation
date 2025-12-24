72782823b58f19a60482d8f18039b934eaabc141
human02 <42168454+human02@users.noreply.github.com>
2025-12-24 13:44:45 -0800

    add ques

---
+++ dsa/linked_list/find_middle_LL.py
@@ -0,0 +1,24 @@
+"""
+
+Find Middle of Linked List
+
+Given the head of a singly Linked List, return the middle node of the Linked List.
+If the Linked List has an even number of nodes, return the second middle one.
+
+Examples:
+Input: head -> 3 -> 8 -> 7 -> 1 -> 3
+Output(value at returned node): 7
+Explanation: There are 5 nodes, so the middle node is the 3rd Node, with value 7.
+
+Input: head -> 2 -> 9 -> 1 -> 4 -> 0 -> 4
+Output(value at returned node): 4
+Explanation: There are 6 nodes, thus both the 3rd and 4th nodes are middle. So the 2nd middle node (4th Node) is returned with value 4.
+
+Input: head -> 3 -> 8 -> 1 -> 7 -> 0
+Output: 1
+
+Constraints:
+1 <= number of Nodes in the Linked List <= 105
+-104 <= ListNode.val <= 104
+
+"""
