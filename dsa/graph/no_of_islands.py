9b17084ad0a678c6ce0852c91cc4d5118353235e
human02 <42168454+human02@users.noreply.github.com>
2025-12-27 23:49:06 -0800

    add soln

--- dsa/graph/no_of_islands.py
+++ dsa/graph/no_of_islands.py
@@ -40,3 +40,66 @@ Constraints:
     ·  grid[i][j] is '0' or '1'.
 
 """
+
+from collections import deque
+
+
+# TC - O(m*n), SC - O(m*n)
+class Solution:
+    def findIslands(self, grid):
+        m = len(grid)
+        n = len(grid[0])
+        visited = [[False for _ in range(n)] for _ in range(m)]
+        island_count = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1" and not visited[i][j]:
+                    island_count += 1
+                    self.bfs(grid, visited, i, j)
+        return island_count
+
+    def isValid(self, i, j, m, n):
+        if i < 0 or i >= m or j < 0 or j >= n:
+            return False
+        return True
+
+    def bfs(self, grid, vis, r, c):
+        vis[r][c] = True
+        q = deque()
+        q.append((r, c))
+        m = len(grid)
+        n = len(grid[0])
+
+        while q:
+            row, col = q.popleft()
+
+            # Traversingin all 8 directions
+            for delRow in range(-1, 2):
+                for delCol in range(-1, 2):
+                    newRow = row + delRow
+                    newCol = col + delCol
+                    if (
+                        self.isValid(newRow, newCol, m, n)
+                        and not vis[newRow][newCol]
+                        and grid[newRow][newCol] == "1"
+                    ):
+                        q.append((newRow, newCol))
+                        vis[newRow][newCol] = True
+        return
+
+
+if __name__ == "__main__":
+    grid = [
+        ["1", "1", "1", "0", "1"],
+        ["1", "0", "0", "0", "0"],
+        ["1", "1", "1", "0", "1"],
+        ["0", "0", "0", "1", "1"],
+    ]
+
+    # Creating an instance of Solution class
+    sol = Solution()
+
+    # Function call to find the number of islands in given grid
+    ans = sol.findIslands(grid)
+
+    print(f"\nThe total islands in given grids are {ans}\n")
