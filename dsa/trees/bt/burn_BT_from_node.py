3e627eb0b3fda5810a214e68687414fb03ad7b07
human02 <42168454+human02@users.noreply.github.com>
2025-12-28 20:45:38 -0800

    add soln

--- dsa/trees/bt/burn_BT_from_node.py
+++ dsa/trees/bt/burn_BT_from_node.py
@@ -37,3 +37,69 @@ Constraints
     All Node.val values are unique.
 
 """
+
+from collections import deque
+
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.value = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def timeToBurn(self, root, start):
+        # create parent mapping
+        q = deque([root])
+        parent_mpp = {}
+        while q:
+            node = q.popleft()
+            # only given the start value not the node with that value, hence finding it.
+            if node.value == start:
+                startNode = node
+            if node.left:
+                parent_mpp[node.left] = node
+                q.append(node.left)
+            if node.right:
+                parent_mpp[node.right] = node
+                q.append(node.right)
+
+        # Radially expand
+        visited = {startNode}
+        q = deque([startNode])
+        dist = 0
+        while q:
+            size = len(q)
+            for _ in range(size):
+                node = q.popleft()
+                if node.left and node.left not in visited:
+                    visited.add(node.left)
+                    q.append(node.left)
+                if node.right and node.right not in visited:
+                    visited.add(node.right)
+                    q.append(node.right)
+                if node in parent_mpp and parent_mpp[node] not in visited:
+                    visited.add(parent_mpp[node])
+                    q.append(parent_mpp[node])
+            dist += 1
+
+        # additional add in the last iteration
+        return dist - 1
+
+
+if __name__ == "__main__":
+    obj = Solution()
+
+    # Create the binary tree
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(7)
+
+    start = 4
+
+    print(obj.timeToBurn(root, start))
