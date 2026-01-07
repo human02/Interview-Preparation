1dac55d2c0dd73f0e7e1996559a39b02713943c1
human02 <42168454+human02@users.noreply.github.com>
2026-01-07 12:00:39 -0800

    add soln

--- dsa/strings/longst_palindromic_substr.py
+++ dsa/strings/longst_palindromic_substr.py
@@ -18,3 +18,42 @@ Constraints:
     s consist of only digits and English letters.
 
 """
+
+
+class Solution:
+    # TC - O(n^3), SC - O(1)
+    def findPalinSubStr_brute(self, s):
+        """
+        Idea:
+        - Find all Substring and check which of them are palindrome
+        - Out of the palindrome strings, return the max length one
+        """
+
+        n = len(s)
+
+        def isPalindrome(s1):
+            i, j = 0, len(s1) - 1
+            while i <= j:
+                if s1[i] != s1[j]:
+                    return False
+                i += 1
+                j -= 1
+
+            return True
+
+        result = ""
+        max_len = 0
+
+        for i in range(n):
+            for j in range(i, n):
+                substring = s[i : j + 1]
+                if isPalindrome(substring) and len(substring) > max_len:
+                    max_len = len(substring)
+                    result = substring
+
+        return result
+
+if __name__ == "__main__":
+    obj = Solution()
+    print(obj.findPalinSubStr_brute("babad"))
+    print(obj.findPalinSubStr_brute("cbbd"))
