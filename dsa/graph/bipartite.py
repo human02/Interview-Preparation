631c5873b3ae30ae5fe7c16ae826fc1ba5560d71
human02 <42168454+human02@users.noreply.github.com>
2026-01-14 09:59:42 -0800

    add ques

---
+++ dsa/graph/bipartite.py
@@ -0,0 +1,32 @@
+"""
+
+Bipartite graph
+
+Given an undirected graph with V vertices labeled from 0 to V-1.
+The graph is represented using an adjacency list where adj[i] lists all nodes connected to node.
+Determine if the graph is bipartite or not.
+
+A graph is bipartite if the nodes can be partitioned into two independent sets A and B such that
+every edge in the graph connects a node in set A and a node in set B.
+
+Example 1
+Input: V=4, adj = [[1,3],[0,2],[1,3],[0,2]]
+Output: True
+Explanation: The given graph is bipartite since, we can partition the nodes into two sets: {0, 2} and {1, 3}.
+
+Example 2
+Input: V=4, adj = [[1,2,3],[0,2],[0,1,3],[0,2]]
+Output: False
+Explanation: The graph is not bipartite.
+    If we attempt to partition the nodes into two sets, we encounter an edge that connects
+    two nodes within the same set, which violates the bipartite property.
+
+Example 3
+Input: V=5, adj = [[1,3],[0,2],[1,4],[0,4],[2,3]]
+Output: False
+
+Constraints
+    E=number of edges
+    1 ≤ V, E ≤ 104
+
+"""
