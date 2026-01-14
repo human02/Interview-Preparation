2f5f748ff7bccca307a4f08c254114361f7f9c14
human02 <42168454+human02@users.noreply.github.com>
2026-01-14 10:00:02 -0800

    add soln

--- dsa/graph/bipartite.py
+++ dsa/graph/bipartite.py
@@ -30,3 +30,63 @@ Constraints
     1 ≤ V, E ≤ 104
 
 """
+
+from collections import deque
+
+
+class Solution:
+    # TC - O(V+E), SC - O(V)
+    def isBipartite(self, V, adj):
+        """
+        Idea:
+        - A graph is bipartite if and only if it is 2-colorable.
+        - Any linear graph with no cycle is a bipartite graph
+        - A graph with cycle:
+            - even length cycle is bipartite
+            - Odd length cycle is NEVER bipartite
+        - To check if a graph is bipartite, check if:
+             - it nodes acan be colored alternatively
+             - if alternative coloring is not possible = not bipartite
+        - Use 'color' insterad of vis array:
+             - -1 means uncolored
+             - 0 and 1 are the 2 diff colors
+        - We can start the BFS with any node, we start with node1.
+        - We color uncolred nei nodes with opp color of node.
+        - If we reach a colored node we check if coloring is correct or not.
+        """
+        color = [-1] * V
+
+        def bfs(src):
+            q = deque()
+
+            q.append(src)  # add start node
+            color[src] = 0  # update color of start node
+
+            # BFS
+            while q:
+                node = q.popleft()
+
+                for it in adj[node]:
+                    # uncolored
+                    if color[it] == -1:
+                        color[it] = 1 - (color[node])
+                        q.append(it)
+                    # if colored, should not be the same as the adjacent color
+                    elif color[it] == color[node]:
+                        return False
+            return True
+
+        # if all node are not connected
+        for i in range(V):
+            if color[i] == -1:
+                result = bfs(i)
+                if not result:
+                    return False
+        return True
+
+
+if __name__ == "__main__":
+    obj = Solution()
+    print(obj.isBipartite(4, [[1, 3], [0, 2], [1, 3], [0, 2]]))
+    print(obj.isBipartite(4, [[1, 2, 3], [0, 2], [0, 1, 3], [0, 2]]))
+    print(obj.isBipartite(5, [[1, 3], [0, 2], [1, 4], [0, 4], [2, 3]]))
