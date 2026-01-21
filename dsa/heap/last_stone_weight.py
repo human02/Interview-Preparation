45646afb7f0c443ec4b63195582790f495b7e13c
human02 <42168454+human02@users.noreply.github.com>
2026-01-20 18:41:18 -0800

    format ques

--- dsa/heap/last_stone_weight.py
+++ dsa/heap/last_stone_weight.py
@@ -4,11 +4,10 @@ Last Stone Weight
 
 You are given an array of integers stones where stones[i] represents the weight of the ith stone.
 We want to run a simulation on the stones as follows:
-
-At each step we choose the two heaviest stones, with weight x and y and smash them togethers
-If x == y, both stones are destroyed
-If x < y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
-Continue the simulation until there is no more than one stone remaining.
+    At each step, we choose the two heaviest stones, with weight x and y and smash them togethers
+    If x == y, both stones are destroyed
+    If x < y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+    Continue the simulation until there is no more than one stone remaining.
 
 Return the weight of the last remaining stone or return 0 if none remain.
 
