a04d54dc68658af262238f6e4836b798ea7c8ada
human02 <42168454+human02@users.noreply.github.com>
2026-01-21 18:31:00 -0800

    add soln

--- dsa/strings/valid_palindrome_I.py
+++ dsa/strings/valid_palindrome_I.py
@@ -38,3 +38,32 @@ Constraints:
     s is made up of only printable ASCII characters.
 
 """
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        n = len(s)
+        left, right = 0, n - 1
+
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
+
+
+if __name__ == "__main__":
+    obj = Solution()
+    print(obj.isPalindrome("Was it a car or a cat I saw?"))
+    print(obj.isPalindrome("tab a cat"))
+    print(obj.isPalindrome("A man, a plan, a canal: Panama"))
+    print(obj.isPalindrome("race a car"))
+    print(obj.isPalindrome(" "))
