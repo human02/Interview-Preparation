1b6fa148e588ddae32374734027a88972b5568f3
human02 <42168454+human02@users.noreply.github.com>
2026-01-22 12:28:26 -0800

    add soln optimal

--- dsa/arrays/flip_monotonic.py
+++ dsa/arrays/flip_monotonic.py
@@ -73,6 +73,32 @@ class Solution:
 
         return minOperation
 
+    def flipToMonotonic_optimal(self, arr):
+        """
+        Idea:
+        - We are re-checking the arr for 1s and 0s at each split again.
+        - We dont need to do it.
+        - Prefix will store how many 1s we have encountered at each split pt.
+        - Maintain a running count of 1s and a var for flips.
+        - Iterate through each num:
+            - Add to 1s count if 1 is seen
+            - If 0 is seen:
+                - either we flip it, flip +=1
+                - or we keep it and flip all the 1s encountered yet
+            - We find the minimum of these two operation
+        """
+        onesCount = 0
+        flips = 0  # flip is a potential flip if 0 found later
+
+        for num in arr:
+            if num == 1:
+                onesCount += 1
+            else:  # 0 found
+                # flip this 0
+                # keep this 0 and flip all prev 1s
+                flips = min(flips + 1, onesCount)
+        return flips
+
 
 if __name__ == "__main__":
     obj = Solution()
@@ -80,3 +106,8 @@ if __name__ == "__main__":
     print(obj.flipToMonotonic_brute([0, 0, 0, 1, 1, 1]))
     print(obj.flipToMonotonic_brute([1, 1, 1, 0, 0, 0]))
     print(obj.flipToMonotonic_brute([1, 0, 0, 0, 0, 0]))
+    print()
+    print(obj.flipToMonotonic_optimal([0, 0, 0, 1, 1, 0]))
+    print(obj.flipToMonotonic_optimal([0, 0, 0, 1, 1, 1]))
+    print(obj.flipToMonotonic_optimal([1, 1, 1, 0, 0, 0]))
+    print(obj.flipToMonotonic_optimal([1, 0, 0, 0, 0, 0]))
