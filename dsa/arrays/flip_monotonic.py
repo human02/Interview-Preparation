1a2cbb6a671fa2dd75ff04b1ba83bba0e62efcaf
human02 <42168454+human02@users.noreply.github.com>
2026-01-22 12:16:34 -0800

    add ques

---
+++ dsa/arrays/flip_monotonic.py
@@ -0,0 +1,28 @@
+"""
+
+Given an array of 0s and 1s. Perform flip operations to make it monotonic.
+Return the minimum operation needed to do it.
+
+1s can be flipped to 0s and vice-versa.
+
+Example 1:
+Input: arr = [0,0,0,1,1,0]
+Output: 1
+Explanation: We flip the last 0 to a 1.
+
+Example 2:
+Input: arr = [0,0,0,1,1,1]
+Output: 0
+
+Example 3:
+Input: arr = [1,1,1,0,0,0]
+Output: 3
+
+Example 4:
+Input: arr = [1,0,0,0,0,0]
+Output: 1
+
+Constraints:
+    Only 0 or 1 in the array
+
+"""
