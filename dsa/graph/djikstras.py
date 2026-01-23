ccac12ad4bea0c53f402844942a1e83613034873
human02 <42168454+human02@users.noreply.github.com>
2026-01-23 00:03:26 -0800

    format ques

--- dsa/graph/djikstras.py
+++ dsa/graph/djikstras.py
@@ -4,6 +4,7 @@ Dijkstra's algorithm
 
 Given a weighted, undirected graph of V vertices, numbered from 0 to V-1, and an adjacency list
 adj where adj[i] represents all edges from vertex i.
+
 Each entry in adj[i] is of the form [to, weight], where:
     to → the neighboring vertex connected to i,
     weight → the weight of the edge between i and to.
@@ -21,17 +22,17 @@ The shortest distance from node 0(source) to node 0 is 0 and the shortest distan
 Input: V = 3,adj = [[[1, 1], [2, 6]], [[2, 3], [0, 1]], [[1, 3], [0, 6]]], S=2
 Output: [4, 3, 0]
 Explanation:
-For node 0, the shortest path is 2->1->0 (distance=4)
-For node 1, the shortest path is 2->1 (distance=3)
+    For node 0, the shortest path is 2->1->0 (distance=4)
+    For node 1, the shortest path is 2->1 (distance=3)
 
 Input: V=4, adj = [[[1, 1], [3, 2]],[[0, 1], [2, 4]],[[1, 4], [3, 3]], [[0, 2], [2, 3]]], S=0
 Output:[0, 1, 5, 2]
 
 Constraints:
-1 ≤ V ≤ 10000
-0 ≤ adj[i][j] ≤ 10000
-1 ≤ adj.size() ≤ [ (V*(V - 1)) / 2 ]
-0 ≤ S < V
+    1 ≤ V ≤ 10000
+    0 ≤ adj[i][j] ≤ 10000
+    1 ≤ adj.size() ≤ [ (V*(V - 1)) / 2 ]
+    0 ≤ S < V
 
 """
 
