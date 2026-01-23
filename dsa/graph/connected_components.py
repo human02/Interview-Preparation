66da5f42ed8e215acbaad9c268d6353e07cb503c
human02 <42168454+human02@users.noreply.github.com>
2026-01-23 00:02:42 -0800

    format ques

--- dsa/graph/connected_components.py
+++ dsa/graph/connected_components.py
@@ -1,6 +1,7 @@
 """
 
 Number of Connected Components in an Undirected Graph
+
 There is an undirected graph with n nodes. There is also an edges array, where edges[i] = [a, b]
 means that there is an edge between node a and node b in the graph.
 The nodes are numbered from 0 to n - 1.
