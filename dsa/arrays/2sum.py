d914732f6a8305b997dfcbaa7755bc3c47d79449
human02 <42168454+human02@users.noreply.github.com>
2026-01-24 15:07:55 -0800

    format ques

--- dsa/arrays/2sum.py
+++ dsa/arrays/2sum.py
@@ -1,5 +1,7 @@
 """
+
 Two Sum
+
 Given an array of integers nums and an integer target, return the indices i and j
 such that nums[i] + nums[j] == target and i != j.
 You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
@@ -19,9 +21,10 @@ Input: nums = [5,5], target = 10
 Output: [0,1]
 
 Constraints:
-2 <= nums.length <= 1000
--10,000,000 <= nums[i] <= 10,000,000
--10,000,000 <= target <= 10,000,000
+    2 <= nums.length <= 1000
+    -10,000,000 <= nums[i] <= 10,000,000
+    -10,000,000 <= target <= 10,000,000
+
 """
 
 
