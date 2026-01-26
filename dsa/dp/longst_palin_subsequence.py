2dfd7d402756f92e5470f6c585fb1a57912354f9
human02 <42168454+human02@users.noreply.github.com>
2026-01-26 13:22:17 -0800

    add soln

--- dsa/dp/longst_palin_subsequence.py
+++ dsa/dp/longst_palin_subsequence.py
@@ -25,3 +25,33 @@ Constraints:
     1 ≤ s.length ≤ 1000
 
 """
+
+
+class Solution:
+    """
+    Idea: Extension of Longest Common Subsequence problem.
+    - Brute way is to find all subsequence, and check which is the longest palindrome.
+    - Optimal way:
+        - Get the reverse of the original string and find Longest Common Subsequence.
+    """
+
+    def findLPS_brute(self, s):
+        rev_s = s[::-1]
+
+        def helper(idx1, idx2):
+            if idx1 < 0 or idx2 < 0:
+                return 0
+
+            if s[idx1] == rev_s[idx2]:
+                return 1 + helper(idx1 - 1, idx2 - 1)
+            else:
+                return max(helper(idx1 - 1, idx2), helper(idx1, idx2 - 1))
+
+        return helper(len(s) - 1, len(s) - 1)
+
+
+if __name__ == "__main__":
+    obj = Solution()
+    print(obj.findLPS_brute("eeeme"))
+    print(obj.findLPS_brute("annb"))
+    print(obj.findLPS_brute("s"))
