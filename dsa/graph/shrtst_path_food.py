463e150458f24596c1b11aca576e69c7ae082dd0
human02 <42168454+human02@users.noreply.github.com>
2026-01-27 00:09:56 -0800

    add soln

--- dsa/graph/shrtst_path_food.py
+++ dsa/graph/shrtst_path_food.py
@@ -45,3 +45,97 @@ Constraints
     The grid contains exactly one ''.
 
 """
+
+from collections import deque
+
+
+class Solution:
+    delRow = [0, 1, 0, -1]
+    delCol = [1, 0, -1, 0]
+
+    # TC - O(m*n), SC - O(m*n)
+    def findShortestPath(self, grid):
+        """
+        Idea:
+        - BFS is the way, undirected graph w/o weight or unit weight.
+        - BFS will give the shortest path.
+        - Step should be tracked in the queue and returned in the end.
+        - Make sure to add to queue:
+            - when valid index
+            - when not visited previously
+            - when grid value is not an obstacle
+        """
+        m = len(grid)
+        n = len(grid[0])
+
+        def isValid(r, c):
+            if r < 0 or r >= m or c < 0 or c >= n:
+                return False
+            return True
+
+        vis = [[0] * n for _ in range(m)]
+        q = deque()
+
+        # Find the start point and add to queue
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "*":
+                    q.append((i, j, 0))  # (row,col), step
+                    vis[i][j] = 1
+
+        while q:
+            r, c, step = q.popleft()
+
+            if grid[r][c] == "#":
+                return step
+
+            for k in range(4):
+                new_r = r + self.delRow[k]
+                new_c = c + self.delCol[k]
+
+                # We want to add if its valid, not obstacle and not visited
+                if (
+                    isValid(new_r, new_c)
+                    and not grid[new_r][new_c] == "X"
+                    and not vis[new_r][new_c]
+                ):
+                    q.append((new_r, new_c, step + 1))
+                    vis[new_r][new_c] = 1
+
+        return -1
+
+
+if __name__ == "__main__":
+    obj = Solution()
+    print(
+        obj.findShortestPath(
+            [
+                ["X", "X", "X", "X", "X", "X"],
+                ["X", "*", "O", "O", "O", "X"],
+                ["X", "O", "O", "#", "O", "X"],
+                ["X", "X", "X", "X", "X", "X"],
+            ]
+        )
+    )
+    print(
+        obj.findShortestPath(
+            [
+                ["X", "X", "X", "X", "X"],
+                ["X", "*", "X", "O", "X"],
+                ["X", "O", "X", "#", "X"],
+                ["X", "X", "X", "X", "X"],
+            ]
+        )
+    )
+
+    print(
+        obj.findShortestPath(
+            [
+                ["X", "X", "X", "X", "X", "X", "X", "X"],
+                ["X", "*", "O", "X", "O", "#", "O", "X"],
+                ["X", "O", "O", "X", "O", "O", "X", "X"],
+                ["X", "O", "O", "O", "O", "#", "O", "X"],
+                ["X", "X", "X", "X", "X", "X", "X", "X"],
+            ]
+        )
+    )
