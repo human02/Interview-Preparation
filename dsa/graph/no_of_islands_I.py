68f555110520eddef54031e0f837ad82a4482cb5
human02 <42168454+human02@users.noreply.github.com>
2026-01-29 11:47:25 -0800

    add soln

--- dsa/graph/no_of_islands_I.py
+++ dsa/graph/no_of_islands_I.py
@@ -40,3 +40,83 @@ Constraints:
     grid[i][j] is '0' or '1'.
 
 """
+
+from collections import deque
+
+
+# TC - O(m*n), SC - O(m*n)
+class Solution:
+    def findIslands(self, grid):
+        m = len(grid)
+        n = len(grid[0])
+
+        def isValid(i, j):
+            if i < 0 or i >= m or j < 0 or j >= n:
+                return False
+            return True
+
+        vis = [[False for _ in range(n)] for _ in range(m)]
+        island_count = 0
+
+        def bfs(r, c):
+            vis[r][c] = True
+            q = deque()
+            q.append((r, c))
+
+            while q:
+                row, col = q.popleft()
+
+                # Traversingin all 8 directions
+                for delRow in range(-1, 2):
+                    for delCol in range(-1, 2):
+                        newRow = row + delRow
+                        newCol = col + delCol
+                        if (
+                            isValid(newRow, newCol)
+                            and not vis[newRow][newCol]
+                            and grid[newRow][newCol] == "1"
+                        ):
+                            q.append((newRow, newCol))
+                            vis[newRow][newCol] = True
+
+        # Will have to start BFS from multiple location
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1" and not vis[i][j]:
+                    island_count += 1
+                    bfs(i, j)
+        return island_count
+
+
+if __name__ == "__main__":
+    obj = Solution()
+    print(
+        obj.findIslands(
+            [
+                ["1", "1", "1", "0", "1"],
+                ["1", "0", "0", "0", "0"],
+                ["1", "1", "1", "0", "1"],
+                ["0", "0", "0", "1", "1"],
+            ]
+        )
+    )
+    print(
+        obj.findIslands(
+            [
+                ["1", "0", "0", "0", "1"],
+                ["0", "1", "0", "1", "0"],
+                ["0", "0", "1", "0", "0"],
+                ["0", "1", "0", "1", " 0"],
+            ]
+        )
+    )
+    print(
+        obj.findIslands(
+            [
+                ["1", "1", "1", "1", "0"],
+                ["1", "1", "0", "1", "0"],
+                ["1", "1", "0", "0", "0"],
+                ["0", "0", "0", "0", "0"],
+            ]
+        )
+    )
