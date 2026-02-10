893287ceb0276ecdf2626f905cc30664e899c1f5
human02 <42168454+human02@users.noreply.github.com>
2026-02-10 13:08:17 -0800

    format ques

--- dsa/arrays/roman_int.py
+++ dsa/arrays/roman_int.py
@@ -12,6 +12,7 @@ L             50
 C             100
 D             500
 M             1000
+
 For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII,
 which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
 
