d8f27d70608890c7b9b1ea6640064bc199230872
human02 <42168454+human02@users.noreply.github.com>
2026-02-14 09:02:36 -0800

    add soln

--- dsa/strings/excel_sheet_col_num.py
+++ dsa/strings/excel_sheet_col_num.py
@@ -33,3 +33,33 @@ Constraints:
     columnTitle is in the range ["A", "FXSHRXW"].
 
 """
+
+
+class Solution:
+    # TC - O(n), SC - O(1)
+    def convertToNum(self, columnTitle):
+        """
+        Idea:
+        - think of it first in base 10:
+            - 1234 to number will be:
+                - (0*10) + 1 = 1
+                - (1*10) + 2 = 12
+                - (12*10) + 3 = 123
+                - (123*10) + 4 = 1234
+            - So its result = 0, then result=result*10 and then result+=s[i]
+        - Now we translate it to base 26 (as only Uppercase characters)
+        - result = 0, result = result*26, result += ord(s[i]) - ord('A') + 1
+        """
+
+        result = 0
+        for _, val in enumerate(columnTitle):
+            result = result * 26
+            result += ord(val) - ord("A") + 1
+        return result
+
+
+if __name__ == "__main__":
+    obj = Solution()
+    print(obj.convertToNum("A"))
+    print(obj.convertToNum("AB"))
+    print(obj.convertToNum("ZY"))
