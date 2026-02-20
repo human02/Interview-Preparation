66ca0738f2ca445bdc1bceef14b96cc4519756dd
human02 <42168454+human02@users.noreply.github.com>
2026-02-20 11:24:09 -0800

    add soln

--- dsa/stack_queues/celebrity_prblem.py
+++ dsa/stack_queues/celebrity_prblem.py
@@ -17,3 +17,41 @@ Output: -1
 Explanation: Both persons know each other, so there is no celebrity.
 
 """
+
+
+class Solution:
+    # TC - O(n^2), SC - O(n)
+    def celebrity_brute(self, M):
+        """
+        Idea:
+        - Keep 2 lists iKnow,knowMe
+        - iterate through each element and update the lists
+        - index with (n-1) - knowMe and 0 - iKnow is the celebrity
+        """
+
+        # Size of given matrix
+        n = len(M)
+
+        # To store count of people who know person of index i
+        knowMe = [0] * n
+
+        # To store count of people who the person of index i knows
+        iKnow = [0] * n
+
+        for i in range(n):
+            for j in range(n):
+                if M[i][j] == 1:
+                    knowMe[j] += 1
+                    iKnow[i] += 1
+
+        for i in range(n):
+            if knowMe[i] == n - 1 and iKnow[i] == 0:
+                return i
+
+        # Return -1 if no celebrity is found
+        return -1
+if __name__ == "__main__":
+    obj = Solution()
+
+    print(obj.celebrity_brute([[0, 1, 1, 0], [0, 0, 0, 0], [1, 1, 0, 0], [0, 1, 1, 0]]))
+    print(obj.celebrity_brute([[0, 1], [1, 0]]))
