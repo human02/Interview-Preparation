dc8e3c0c2de5a0f3f3ed5682ff66ef391cdd68a6
human02 <42168454+human02@users.noreply.github.com>
2026-02-24 22:13:28 -0800

    add soln

--- dsa/strings/longst_rpt_chr_rplcmnt.py
+++ dsa/strings/longst_rpt_chr_rplcmnt.py
@@ -21,3 +21,48 @@ Constraints:
     0 <= k <= s.length
 
 """
+
+
+class Solution:
+    # TC - O(26n) = O(n), SC - O(1)
+    def characterReplacement(self, s: str, k: int) -> int:
+        """
+        Idea:
+        - at any point, max freq of most occuring element is longest repeating character.
+        - Number of changes needed = len(current substr) - maxFreq of the most occurring char.
+        - Iterate using two pointers:
+            - right moves and we keep calculating curr_max_freq.
+            - if len(substr) - curr_max_freq > k:
+                - we will reduce substr by moving left and recalculate curr_max_freq.
+            - If condition of 'k' is met, we compare it and overwrite maxi.
+        - our result is stored in maxi.
+        """
+        # 256 taken due to ASCII constraint
+        mpp = [0] * 26
+        # Idea --> changes = len(substr)-maxfreq
+        left, right, maxi, curr_max = 0, 0, 0, 0
+
+        while right < len(s):
+            mpp[ord(s[right]) - ord("A")] += 1
+            curr_max = max(curr_max, mpp[ord(s[right]) - ord("A")])
+
+            # substring reduction by left pointer movement
+            while (right - left + 1) - curr_max > k:
+                mpp[ord(s[left]) - ord("A")] -= 1
+                left += 1
+
+                # recalculate curr_max as window is reduced/can be skipped for optimization
+                curr_max = 0
+                for i in range(26):
+                    curr_max = max(curr_max, mpp[i])
+
+            if (right - left + 1) - curr_max <= k:
+                maxi = max(maxi, right - left + 1)
+            right += 1
+        return maxi
+
+
+if __name__ == "__main__":
+    obj = Solution()
+    print(obj.characterReplacement("XYYX", 2))
+    print(obj.characterReplacement("AAABABB", 1))
