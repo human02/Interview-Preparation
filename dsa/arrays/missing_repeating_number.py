44e95320ac3ad9ec9bc1fad7a83e39160ba7810a
human02 <42168454+human02@users.noreply.github.com>
2026-03-04 23:58:27 -0800

    add soln

--- dsa/arrays/missing_repeating_number.py
+++ dsa/arrays/missing_repeating_number.py
@@ -27,3 +27,40 @@ Constraints:
     1 element in nums appears twice, and is valued between [1, n].
 """
 
+
+class Solution:
+    def findMissingRepeatingNumbers(self, nums):
+        n = len(nums)
+
+        # X is repeating number and Y is missing
+        s_n = (n * (n + 1)) // 2
+        s2_n = (n * (n + 1) * (2 * n + 1)) // 6
+
+        s, s2 = 0, 0
+        for num in nums:
+            s += num
+            s2 += num * num
+
+        # val1 = s - s_n = X - Y
+        val1 = s - s_n
+
+        # val2 = s2-s2_n = (X-Y)(X+Y)
+        val2 = s2 - s2_n
+
+        # val2 becomes X+Y
+        val2 = val2 // val1
+
+        # (X+Y)+(X-Y) = 2X
+        x = (val1 + val2) // 2
+        y = x - val1
+
+        return [int(x), int(y)]
+
+
+if __name__ == "__main__":
+    nums = [3, 1, 2, 5, 4, 6, 7, 5]
+
+    sol = Solution()
+    result = sol.findMissingRepeatingNumbers(nums)
+
+    print(f"\nThe repeating and missing numbers are: {{{result[0]}, {result[1]}}}\n")
