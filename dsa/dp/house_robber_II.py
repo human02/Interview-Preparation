b633c65d6108d1fc2dccd89253054b653d9f5c80
human02 <42168454+human02@users.noreply.github.com>
2026-03-08 19:13:42 -0700

    add soln recur

--- dsa/dp/house_robber_II.py
+++ dsa/dp/house_robber_II.py
@@ -39,3 +39,44 @@ Constraints:
     0 <= nums[i] <= 100
 
 """
+
+
+class Solution:
+    # TC - O(2^n + 2^n), SC - O(n)
+    def robHouses_recur(self, money):
+        n = len(money)
+        arr1 = []  # wont have 1st house
+        arr2 = []  # wont have last house
+
+        if n == 1:
+            return money[0]
+
+        for i in range(n):
+            if i != 0:
+                arr1.append(money[i])
+            if i != (n - 1):
+                arr2.append(money[i])
+
+        def helper(ind, arr):
+            if ind == 0:
+                return arr[0]
+            if ind < 0:
+                return 0
+
+            pick, not_pick = 0, 0
+            pick = helper(ind - 1, arr)
+            not_pick = arr[ind] + helper(ind - 2, arr)
+
+            return max(pick, not_pick)
+
+        ans1 = helper(len(arr1) - 1, arr1)
+        ans2 = helper(len(arr2) - 1, arr2)
+
+        return max(ans1, ans2)
+
+if __name__ == "__main__":
+    obj = Solution()
+    print(obj.robHouses_recur([2, 1, 4, 9]))
+    print(obj.robHouses_recur([1, 5, 2, 1, 6]))
+    print(obj.robHouses_recur([2, 9, 8, 3, 6]))
+    print(obj.robHouses_recur([3, 4, 3]))
